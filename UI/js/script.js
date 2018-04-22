678806e2e9b0065034bb7fc7c1d0496cc49ca44e
Babatunde <soft-sky@live.co.uk>
2018-04-22 09:07:04 +0100

    Modified script.js - added displayTabBox function -added removeTabBoxes function -added document onload event Handler
    Deleted "images/fish-chips.jpg"

--- UI/js/script.js
+++ UI/js/script.js
@@ -13,3 +13,75 @@ function closenav () {
     sidenav.style.transform = "translateX(-100%)";
     
 }
+
+function displayArrowUp (id) {
+    const arrowUp = document.getElementById(id);
+    arrowUp.style.display = "block";
+    
+}
+
+function hideArrowUp (id) {
+    const arrowUp = document.getElementById(id);
+    arrowUp.style.display = "none";
+    
+}
+
+function displayTabBox(id, tablink) {
+    removeTabBoxes();
+    const Tab= document.getElementById(id);
+    const tab = document.getElementById(tablink);
+    console.log(Tab);
+    Tab.style.display = "block";
+    tab.classList.add("active");
+}
+
+function removeTabBoxes(){
+    const Tabs = document.getElementsByClassName("mealtabs");
+    const tooldrop = document.getElementsByClassName("tool-drop");
+    const len = Tabs.length;
+
+    let counter = 0;
+    for (counter; counter < len; counter++){
+        Tabs[counter].style.display = "none";
+        console.log(counter);
+        tooldrop[counter].classList.remove("active");
+    }
+
+}
+
+
+ document.onload = displayTabBox("add-meal-content", 1);
+
+ function getFeedBack(type) {
+    const backdrop = document.getElementById("meal-backdrop");
+    const message = document.getElementById("action-message");
+
+    backdrop.style.display = "block";
+    if (type === "remove"){
+        message.innerHTML = "Meal sucessfully removed"
+    } else if (type === "add") {
+        message.innerHTML = "Meal sucessfully added"
+    } else if (type === "modify") {
+        message.innerHTML = "Meal sucessfully Updated"
+    }
+    
+
+ }
+
+
+ function closeBackdrop () {
+    const backdrop = document.getElementById("meal-backdrop");
+    const message = document.getElementById("action-message");
+
+    backdrop.style.display = "none";
+    message.innerHTML = "";
+ }
+
+
+ const showModal = (id) => {
+	return document.getElementById(id).style.display = "block";
+};
+
+const closeModal = (id) => {
+	return document.getElementById(id).style.display = "none";
+};
