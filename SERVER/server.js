7fa83c89e74aa49de2cdc5fbd8438b7e4fdb64bd
Babatunde <soft-sky@live.co.uk>
2018-04-23 19:00:38 +0100

    Setup the server side of the application using

---
+++ SERVER/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import openRoutes from './src/route/user';
+import securedRoutes from './src/route/secure-user';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('short'));
+
+app.use(bodyParser.urlencoded({ extended: false, type: '*/x-www-form-urlencoded' }));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.text({ type: 'text/plain' }));
+app.use(bodyParser.raw({ type: '*/octet-stream' }));
+
+
+app.use('/api/v1/user', openRoutes);
+
+app.use((err, req, res, next) => {
+    if(err.status == 404){
+        res.status(404).send('Not Found');
+    }
+    next();
+});
+
+app.use((err,req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
+
+app.all('*', (req, res) => {
+    res.send('Page Not Found');
+})
+app.listen(port, () => {
+    console.log("Server is listening on port "+ port);
+})
+
+export default app;
