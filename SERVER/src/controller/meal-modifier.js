7bea04a849f15fb2cab390885f207fd5c56a7d7b
Babatunde <soft-sky@live.co.uk>
2018-04-24 21:50:39 +0100

    Update the information of a meal option

--- SERVER/src/controller/meal-modifier.js
+++ SERVER/src/controller/meal-modifier.js
@@ -1,6 +1,24 @@
+import { data } from '../db/data';
+
 class MealModifier {
   static modifyAMeal(req, res) {
+    const {
+      name, description, price, category,
+    } = req.body;
 
+    const id = req.params.mealId;
+    // console.log(id);
+
+    const mealIndex = data.meals.findIndex(meal => meal.id === +id);
+    if (mealIndex !== undefined) {
+      data.meals[mealIndex].name = name;
+      data.meals[mealIndex].description = description;
+      data.meals[mealIndex].price = price;
+      data.meals[mealIndex].category = category;
+      res.status(200).json({ msg: 'meal modified successfully' });
+    } else {
+      res.status(404).json({ msg: 'meal does not exist' });
+    }
   }
 }
 
