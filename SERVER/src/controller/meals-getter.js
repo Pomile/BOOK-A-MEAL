dca64eac7e8d0b384afcaa942da7181604880bb4
Babatunde <soft-sky@live.co.uk>
2018-04-25 09:27:25 +0100

    Setup menu for the day [finishes #156825586]

--- SERVER/src/controller/meals-getter.js
+++ SERVER/src/controller/meals-getter.js
@@ -2,7 +2,6 @@ import { data } from '../db/data';
 
 class MealsGetter {
   static getMeals(req, res) {
-    console.log('get meal method');
     const mealList = data.meals;
     res.status(200).json({ data: mealList, sucess: true });
   }
--- SERVER/src/controller/menu-creator.js
+++ SERVER/src/controller/menu-creator.js
@@ -3,15 +3,13 @@ import { data } from '../db/data';
 class MenuCreator {
   static addAMenu(req, res) {
     const selectedMeals = req.body.meals;
-    // console.log(selectedMeals);
+
     const menu = data.meals.map((meal) => {
       if (selectedMeals.includes(meal.id)) {
         return meal;
       }
       return null;
     }).filter(item => item !== null);
-    // console.log(data.meals);
-    // console.log(menu);
 
     const date = new Date();
     const month = (date.getMonth() + 1).toString();
@@ -19,10 +17,10 @@ class MenuCreator {
     const year = date.getFullYear().toString();
     const todaysDate = `${month}/${day}/${year}`;
     const menuLen = data.menus.length;
-    // search if menu is already added for the day
 
+    // search if menu is already added for the day
     const searchTodaysMenu = data.menus.findIndex(currentmenu => currentmenu.date === todaysDate);
-    // console.log(`index exist or not: ${searchTodaysMenu}`);
+
     if (searchTodaysMenu === -1) {
       data.menus.push({
         id: menuLen + 1,
