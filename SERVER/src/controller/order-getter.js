ada3f95f4aafacceffceefb26c3c228b5a546678
Babatunde <soft-sky@live.co.uk>
2018-04-25 21:31:22 +0100

    Order modifier API [Finishes #157090496]

--- SERVER/src/controller/order-getter.js
+++ SERVER/src/controller/order-getter.js
@@ -2,22 +2,7 @@ import { data } from '../db/data';
 
 class OrderGetter {
   static getOrder(req, res) {
-    // console.log(req.user);
-    const { email } = req.user;
-    const findOrdersByCustomerName = data.orders.map((order) => {
-      if (order.username === email) {
-        return order;
-      }
-      return null;
-    })
-      .filter(item => item !== null);
 
-    if (findOrdersByCustomerName) {
-      console.log(findOrdersByCustomerName);
-      res.status(200).json({ success: true, data: findOrdersByCustomerName });
-    } else {
-      res.status(404).json({ msg: 'Order history cannot be found' });
-    }
   }
 }
 
