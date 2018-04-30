acfaacf5204a05fa1cddc88f872848ae182d4223
Babatunde <soft-sky@live.co.uk>
2018-04-30 04:47:22 +0100

    Coverage Test

--- SERVER/src/controller/orders-getter.js
+++ SERVER/src/controller/orders-getter.js
@@ -11,7 +11,6 @@ class OrdersGetter {
     }).filter(item => item !== null);
     if (findOrdersByDate.length > 0) {
       const sumTotalOfOrders = findOrdersByDate.reduce((sum, order) => sum + order.price, 0);
-      console.log(sumTotalOfOrders);
       res.status(200).json({ data: findOrdersByDate, total: sumTotalOfOrders, success: true });
     } else {
       res.status(404).json({ msg: `No Order(s) is available for ${specifiedDate} `, success: false });
