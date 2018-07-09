65a170788d620aa980236a9185c129a359d54633
Babatunde <soft-sky@live.co.uk>
2018-07-09 01:30:41 +0100

    Modified environment variable to run an instance of the app on heroku

--- SERVER/src/models/index.js
+++ SERVER/src/models/index.js
@@ -12,7 +12,7 @@ config.operatorsAliases = operatorsAliases;
 const db = {};
 
 let sequelize;
-if (config.environment === 'production') {
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
